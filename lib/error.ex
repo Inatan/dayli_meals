0fb5b376d2955f714598b8a59a868644f2a255e7
Inatan Lopes Hertzog <inatan.hertzog@conectt.com.br>
2021-08-31 15:39:50 -0300

    Init challenge

--- lib/error.ex
+++ lib/error.ex
@@ -1,3 +1,14 @@
 defmodule Exmeal.Error do
-# TO DO
+  @keys [:status, :result]
+
+  @enforce_keys @key
+
+  defstruct @keys
+
+  def build(status, result) do
+    %__MODULE__{
+      status: status,
+      result: result
+    }
+  end
 end
